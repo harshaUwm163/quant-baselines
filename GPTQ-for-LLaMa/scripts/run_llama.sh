2145d7404e72310cce5e9b779d02c2d3a826f3c9
harshaUwm163 <adepu@wisc.edu>
2024-05-15 23:02:39 +0000

    modified script to take 6 bits

--- GPTQ-for-LLaMa/scripts/run_llama.sh
+++ GPTQ-for-LLaMa/scripts/run_llama.sh
@@ -1,15 +1,16 @@
 llama_model="llama-3-8b" # "llama-3-8b", "llama-3-70b"
+echo num_mlp_bits = "$1"
 python llama.py /data/$llama_model \
                 c4 \
                 --eval \
                 --new-eval \
                 --attn_wbits 8 \
-                --mlp_wbits 2 \
+                --mlp_wbits "$1" \
                 --true-sequential \
                 --act-order \
                 --parent_dir $llama_model \
                 --groupsize 128 \
-                --exp_name gptq82_act_g128 \
+                --exp_name gptq8"$1"_act_g128 \
                 --save packed_model.pt \
                 --seqlen 8192
 
